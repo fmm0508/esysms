c0b997304570fb96e4ba0cd9414b80040b67b415
Farahani Mahendo <farahani.mahendo@rwjbh.org>
2022-06-11 20:46:35 -0400

    Rewrite service workers

---
+++ src/Patient.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Patient() {
+  return (
+    <div>
+      <center>
+        <p>This is the patient you transport</p>
+        <h3>Harry Porter</h3>
+      </center>
+    </div>
+  );
+}
+
+export default Patient;
---
+++ src/services/onLoad.js
@@ -0,0 +1,53 @@
+// window.addEventListener('load', function() {
+//   subscribe();
+//   if ('serviceWorker' in navigator) {
+//   navigator.serviceWorker.register('/service-worker.js')
+//   .then(initialiseState);
+//   } else {
+//   console.warn('Service workers aren\'t supported in this browser.');
+//   }
+// });
+
+function initialiseState() {
+  if (!('showNotification' in ServiceWorkerRegistration.prototype)) {
+    console.warn('Notifications aren\'t supported.');
+    return;
+  }
+  if (Notification.permission === 'denied') {
+    console.warn('The user has blocked notifications.');
+    return;
+  }
+  if (!('PushManager' in window)) {
+  console.warn('Push messaging isn\'t supported.');
+  return;
+  }
+  navigator.serviceWorker.ready.then(function(serviceWorkerRegistration) {
+  serviceWorkerRegistration.pushManager.getSubscription()
+    .then(function(subscription) {
+      if (!subscription) {
+        return;
+      }
+    }).catch(function(err) {
+      console.warn('Error during getSubscription()', err);
+    });
+  });
+}
+
+function subscribe() {
+  navigator.serviceWorker.ready.then(function(serviceWorkerRegistration) {
+  serviceWorkerRegistration.pushManager.subscribe()
+    .then(function(subscription) {
+      console.log(subscription);
+    //return sendSubscriptionToServer(subscription);
+    })
+    .catch(function(e) {
+    if (Notification.permission === 'denied') {
+        console.warn('Permission for Notifications was denied');
+    } else {
+        console.error('Unable to subscribe to push.', e);
+    }
+    });
+  });
+}
+
+export { subscribe, initialiseState };
