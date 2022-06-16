b0b3b0199aca9629ad261d24a1fb77666b389ae0
Farahani Mahendo <farahani.mahendo@rwjbh.org>
2022-06-16 13:04:52 -0400

    Remove files

--- src/App.js
+++ src/App.js
@@ -1,21 +1,10 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
-import Login from "./Login";
-import Patient from "./Patient";
-import AddPatient from "./backend/AddPatient";
 
 
 function App() {
   return (
-    <center>
-    <Router>
-      <Routes>
-        <Route exact path="/" element={<Login />} />
-        <Route exact path="/patient" element={<Patient />} />
-        <Route exact path="/backend" element={<AddPatient />} />
-      </Routes>
-    </Router>
-    </center>
+    <div><p>Yes</p></div>
   );
 }
 
